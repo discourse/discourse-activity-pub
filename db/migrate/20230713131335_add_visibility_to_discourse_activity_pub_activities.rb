8c993d20deaf7d16999c5d8d5c70ad5e2e10ae6d
Angus McLeod <angus@mcleod.org.au>
2023-07-28 12:47:32 +0200

    Change visibility database default to public

--- db/migrate/20230713131335_add_visibility_to_discourse_activity_pub_activities.rb
+++ db/migrate/20230713131335_add_visibility_to_discourse_activity_pub_activities.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddVisibilityToDiscourseActivityPubActivities < ActiveRecord::Migration[7.0]
   def change
-    add_column :discourse_activity_pub_activities, :visibility, :integer, default: 1
+    add_column :discourse_activity_pub_activities, :visibility, :integer, default: 2
   end
 end
