68402b6b69d7258c86845ec833b36a76f05e5938
Angus McLeod <angus@mcleod.org.au>
2023-10-31 17:16:38 +0800

    Improve category_controller readability

--- app/controllers/discourse_activity_pub/category_controller.rb
+++ app/controllers/discourse_activity_pub/category_controller.rb
@@ -14,53 +14,62 @@ module DiscourseActivityPub
     def followers
       guardian.ensure_can_see!(@category)
 
-      permitted_order = ORDER.find { |attr| attr == params[:order] }
-      order = case permitted_order
-              when 'actor' then 'username'
-              when 'user' then 'username'
-              when 'followed_at' then 'created_at'
-              else 'created_at'
-              end
-      order_table = case permitted_order
-                    when 'actor' then 'discourse_activity_pub_actors'
-                    when 'user' then 'users'
-                    when 'followed_at' then 'discourse_activity_pub_follows'
-                    else 'discourse_activity_pub_follows'
-                    end
-      order_dir = params[:asc] ? "ASC" : "DESC"
-
       followers = @category
         .activity_pub_followers
         .joins(:follow_follows)
         .where(follow_follows: { followed_id: @category.activity_pub_actor.id })
         .joins(:user)
-        .order("#{order_table}.#{order} #{order_dir}")
+        .order("#{order_table}.#{order} #{params[:asc] ? "ASC" : "DESC"}")
 
       limit = fetch_limit_from_params(default: PAGE_SIZE, max: PAGE_SIZE)
       page = fetch_int_from_params(:page, default: 0)
       total = followers.count
       followers = followers.limit(limit).offset(limit * page).to_a
 
-      load_more_params = params.slice(:order, :asc).permit!
-      load_more_params[:page] = page + 1
-      load_more_uri = ::URI.parse("/ap/category/#{params[:category_id]}/followers.json")
-      load_more_uri.query = ::URI.encode_www_form(load_more_params.to_h)
-
-      serialized = serialize_data(followers, FollowerSerializer, root: false)
       render_json_dump(
-        followers: serialized,
+        followers: serialize_data(followers, FollowerSerializer, root: false),
         meta: {
           total: total,
-          load_more_url: load_more_uri.to_s,
+          load_more_url: load_more_url(page),
         }
       )
     end
 
     protected
 
+    def permitted_order
+      @permitted_order ||= ORDER.find { |attr| attr == params[:order] }
+    end
+
+    def order_table
+      case permitted_order
+      when 'actor' then 'discourse_activity_pub_actors'
+      when 'user' then 'users'
+      when 'followed_at' then 'discourse_activity_pub_follows'
+      else 'discourse_activity_pub_follows'
+      end
+    end
+
+    def order
+      case permitted_order
+      when 'actor' then 'username'
+      when 'user' then 'username'
+      when 'followed_at' then 'created_at'
+      else 'created_at'
+      end
+    end
+
+    def load_more_url(page)
+      load_more_params = params.slice(:order, :asc).permit!
+      load_more_params[:page] = page + 1
+      load_more_uri = ::URI.parse("/ap/category/#{params[:category_id]}/followers.json")
+      load_more_uri.query = ::URI.encode_www_form(load_more_params.to_h)
+      load_more_uri.to_s
+    end
+
     def find_category
       @category = Category.find_by_id(params.require(:category_id))
-      return render_category_error("category_not_found", 400) unless @category.present?
+      render_category_error("category_not_found", 400) unless @category.present?
     end
 
     def ensure_site_enabled
