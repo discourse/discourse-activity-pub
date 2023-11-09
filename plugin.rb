ed1a00e66712022ab31c9cff939fa932076b2f71
Bianca Nenciu <nbianca@users.noreply.github.com>
2023-11-09 18:30:51 +0200

    DEV: Use new API to preload category custom fields

--- plugin.rb
+++ plugin.rb
@@ -221,17 +221,16 @@ after_initialize do
     include_condition: -> { object.activity_pub_enabled }
   ) { object.activity_pub_ready? }
 
-  if Site.respond_to? :preloaded_category_custom_fields
+  if respond_to?(:register_preloaded_category_custom_fields)
+    register_preloaded_category_custom_fields("enable_unassigned_filter")
+  else
+    # TODO: Drop the if-statement and this if-branch in Discourse v3.2
     Site.preloaded_category_custom_fields << "activity_pub_enabled"
     Site.preloaded_category_custom_fields << "activity_pub_ready"
   end
 
   register_modifier(:site_all_categories_cache_query) do |query|
     query.includes(:activity_pub_actor)
-  end
-
-  if self.respond_to?(:register_category_list_preloaded_category_custom_fields)
-    register_category_list_preloaded_category_custom_fields("activity_pub_enabled")
   end
 
   serialized_category_custom_fields = %w(
