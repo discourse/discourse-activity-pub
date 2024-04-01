c98c087fd191a80ecc83c28a20a237687690cc01
Angus McLeod <angus@mcleod.org.au>
2024-04-01 12:41:25 +0200

    Remove rows with duplicate ap_id before adding uniqueness constraint

--- db/migrate/20240222124517_add_uniqueness_to_discourse_activity_pub_model_associations.rb
+++ db/migrate/20240222124517_add_uniqueness_to_discourse_activity_pub_model_associations.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 class AddUniquenessToDiscourseActivityPubModelAssociations < ActiveRecord::Migration[7.0]
-  def change
+  def up
     remove_index :discourse_activity_pub_activities, %i[ap_id]
     remove_index :discourse_activity_pub_collections, %i[ap_id]
     remove_index :discourse_activity_pub_actors, %i[ap_id]
     remove_index :discourse_activity_pub_objects, %i[ap_id]
+
+    # Remove duplicates
+    execute "DELETE FROM discourse_activity_pub_actors WHERE id IN (SELECT id FROM (SELECT id, row_number() over (PARTITION BY ap_id ORDER BY model_id IS NULL, model_id DESC) AS rnum FROM discourse_activity_pub_actors) t WHERE t.rnum > 1)"
+    execute "DELETE FROM discourse_activity_pub_activities WHERE id IN (SELECT id FROM (SELECT id, row_number() over (PARTITION BY ap_id ORDER BY object_id IS NULL, object_id DESC) AS rnum FROM discourse_activity_pub_activities) t WHERE t.rnum > 1)"
+    execute "DELETE FROM discourse_activity_pub_collections WHERE id IN (SELECT id FROM (SELECT id, row_number() over (PARTITION BY ap_id ORDER BY model_id IS NULL, model_id DESC) AS rnum FROM discourse_activity_pub_collections) t WHERE t.rnum > 1)"
+    execute "DELETE FROM discourse_activity_pub_objects WHERE id IN (SELECT id FROM (SELECT id, row_number() over (PARTITION BY ap_id ORDER BY model_id IS NULL, model_id DESC) AS rnum FROM discourse_activity_pub_objects) t WHERE t.rnum > 1)"
+
     add_index :discourse_activity_pub_actors, %i[ap_id], unique: true
     add_index :discourse_activity_pub_collections, %i[ap_id], unique: true
     add_index :discourse_activity_pub_activities, %i[ap_id], unique: true
@@ -21,5 +28,25 @@ class AddUniquenessToDiscourseActivityPubModelAssociations < ActiveRecord::Migra
               %i[model_type model_id],
               unique: true,
               name: "unique_activity_pub_object_models"
+  end
+
+  def down
+    remove_index :discourse_activity_pub_actors, %i[ap_id]
+    remove_index :discourse_activity_pub_collections, %i[ap_id]
+    remove_index :discourse_activity_pub_activities, %i[ap_id]
+    remove_index :discourse_activity_pub_objects, %i[ap_id]
+    remove_index :discourse_activity_pub_collections,
+                 %i[model_type model_id],
+                 name: "unique_activity_pub_collection_models"
+    remove_index :discourse_activity_pub_actors,
+                 %i[model_type model_id],
+                 name: "unique_activity_pub_actor_models"
+    remove_index :discourse_activity_pub_objects,
+                 %i[model_type model_id],
+                 name: "unique_activity_pub_object_models"
+    add_index :discourse_activity_pub_activities, %i[ap_id]
+    add_index :discourse_activity_pub_collections, %i[ap_id]
+    add_index :discourse_activity_pub_actors, %i[ap_id]
+    add_index :discourse_activity_pub_objects, %i[ap_id]
   end
 end
