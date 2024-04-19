75d0f551016d2c0d0f2fb4f7b07e2d3bdbfcef92
Angus McLeod <angus@mcleod.org.au>
2024-04-19 11:54:50 +0200

    Move to admin plugins parent route

--- assets/javascripts/discourse/controllers/admin-activity-pub.js
+++
@@ -1,6 +0,0 @@
-import Controller from "@ember/controller";
-import { newActor } from "../models/activity-pub-actor";
-
-export default class adminActivityPub extends Controller {
-  newActor = newActor;
-}
