8f327eee3b03ef21638731acf95fef8091154a8e
Penar Musaraj <pmusaraj@gmail.com>
2024-09-09 16:23:19 -0400

    FIX: Adding tags in admin UI (#116)
    This was broken after an upgrade in core that refactored the TagDrop
    component. This PR switches to using the TagChooser component, since it
    requires no overrides.

--- test/javascripts/acceptance/activity-pub-admin-test.js
+++ test/javascripts/acceptance/activity-pub-admin-test.js
@@ -165,6 +165,86 @@ acceptance("Discourse Activity Pub | Admin | New Actor", function (needs) {
 
     await click(".activity-pub-save-actor");
   });
+
+  test("creates a new actor with a tag model", async function (assert) {
+    await visit("/admin/plugins/ap/actor/new");
+
+    assert.ok(
+      exists(".activity-pub-actor-add"),
+      "add actor container is visible"
+    );
+    assert.ok(
+      exists(".activity-pub-new-actor-model"),
+      "actor model controls are visible"
+    );
+
+    const modelTypes = selectKit(".activity-pub-model-type-chooser");
+    await modelTypes.expand();
+    await modelTypes.selectRowByValue("tag");
+
+    const tags = selectKit(".activity-pub-actor-add .tag-chooser");
+    await tags.expand();
+    await tags.selectRowByName("monkey");
+
+    assert.ok(
+      exists(".activity-pub-actor-form"),
+      "activity pub actor form is visible"
+    );
+
+    const actor = {
+      username: "ap_monkey",
+      name: "AP monkey",
+      default_visibility: "public",
+      post_object_type: "Article",
+      publication_type: "full_topic",
+      model_type: "Tag",
+      model_name: "monkey",
+    };
+
+    await fillIn("#activity-pub-username", actor.username);
+    await fillIn("#activity-pub-name", actor.name);
+
+    const visibilities = selectKit(".activity-pub-visibility-dropdown");
+    await visibilities.expand();
+    await visibilities.selectRowByValue(actor.default_visibility);
+
+    const postObjectTypes = selectKit(
+      ".activity-pub-post-object-type-dropdown"
+    );
+    await postObjectTypes.expand();
+    await postObjectTypes.selectRowByValue(actor.post_object_type);
+
+    const publicationTypes = selectKit(
+      ".activity-pub-publication-type-dropdown"
+    );
+    await publicationTypes.expand();
+    await publicationTypes.selectRowByValue(actor.publication_type);
+
+    const createdActor = {
+      ...{ id: 5 },
+      ...actor,
+    };
+
+    pretender.post("/admin/plugins/ap/actor", (request) => {
+      const body = parsePostData(request.requestBody);
+      Object.keys(actor).forEach((attr) => {
+        assert.strictEqual(
+          body.actor[attr],
+          actor[attr],
+          `it posts the correct ${attr}`
+        );
+      });
+      return response({ success: true, actor: createdActor });
+    });
+
+    pretender.get(`/admin/plugins/ap/actor/${createdActor.id}`, () => {
+      return response(createdActor);
+    });
+
+    await click(".activity-pub-save-actor");
+
+    // pauseTest();
+  });
 });
 
 acceptance("Discourse Activity Pub | Admin | Edit Actor", function (needs) {
