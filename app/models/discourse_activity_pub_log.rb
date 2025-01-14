10d9a0605de1e2b28addf33776bf8b309e5fa6f8
Angus McLeod <angus@mcleod.org.au>
2025-01-14 15:46:28 +0100

    Add log annotation

--- app/models/discourse_activity_pub_log.rb
+++ app/models/discourse_activity_pub_log.rb
@@ -6,3 +6,15 @@ class DiscourseActivityPubLog < ActiveRecord::Base
   validates :message, presence: true
   validates :level, presence: true
 end
+
+# == Schema Information
+#
+# Table name: discourse_activity_pub_logs
+#
+#  id         :bigint           not null, primary key
+#  level      :integer
+#  message    :string
+#  json       :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
