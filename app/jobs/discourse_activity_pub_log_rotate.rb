7641c5fc51294b7f1edf1e0dad6737d32856e1e5
Angus McLeod <angus@mcleod.org.au>
2025-01-22 16:00:20 +0100

    Don't make log rotate subject to enabled setting

--- app/jobs/discourse_activity_pub_log_rotate.rb
+++ app/jobs/discourse_activity_pub_log_rotate.rb
@@ -5,8 +5,6 @@ module Jobs
     every 1.day
 
     def execute(args)
-      return if !SiteSetting.activity_pub_enabled
-
       DiscourseActivityPubLog.where(
         "created_at < ?",
         SiteSetting.activity_pub_logs_max_days_old.days.ago,
--- spec/jobs/discourse_activity_pub_log_rotate_spec.rb
+++ spec/jobs/discourse_activity_pub_log_rotate_spec.rb
@@ -5,23 +5,11 @@ RSpec.describe Jobs::DiscourseActivityPubLogRotate do
   let!(:log2) { Fabricate(:discourse_activity_pub_log, created_at: 10.days.ago) }
   let!(:log3) { Fabricate(:discourse_activity_pub_log, created_at: 90.days.ago) }
 
-  context "without activity pub enabled" do
-    before { SiteSetting.activity_pub_enabled = false }
-
-    it "does not destroy logs" do
-      expect { described_class.new.execute({}) }.not_to change { DiscourseActivityPubLog.count }
-    end
-  end
-
-  context "with activity pub enabled" do
-    before { SiteSetting.activity_pub_enabled = true }
-
-    it "destroys logs older than activity_pub_logs_max_days_old" do
-      SiteSetting.activity_pub_logs_max_days_old = 7
-      described_class.new.execute({})
-      expect(DiscourseActivityPubLog.exists?(log1.id)).to eq(true)
-      expect(DiscourseActivityPubLog.exists?(log2.id)).to eq(false)
-      expect(DiscourseActivityPubLog.exists?(log3.id)).to eq(false)
-    end
+  it "destroys logs older than activity_pub_logs_max_days_old" do
+    SiteSetting.activity_pub_logs_max_days_old = 7
+    described_class.new.execute({})
+    expect(DiscourseActivityPubLog.exists?(log1.id)).to eq(true)
+    expect(DiscourseActivityPubLog.exists?(log2.id)).to eq(false)
+    expect(DiscourseActivityPubLog.exists?(log3.id)).to eq(false)
   end
 end
