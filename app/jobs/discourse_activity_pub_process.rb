6a023593e9807375481e82d59e81c5aa386a23e4
Angus McLeod <angus@mcleod.org.au>
2025-02-07 21:28:38 +0100

    Add distributed mutex for AP processing (#158)

--- app/jobs/discourse_activity_pub_process.rb
+++ app/jobs/discourse_activity_pub_process.rb
@@ -4,16 +4,29 @@ module Jobs
   class DiscourseActivityPubProcess < ::Jobs::Base
     def execute(args)
       @args = args
-      ap_activity = DiscourseActivityPub::AP::Activity.factory(@args[:json])
 
-      if ap_activity && ap_activity.respond_to?(:process)
-        ap_activity.delivered_to << @args[:delivered_to] if @args[:delivered_to].present?
-        log_process_start
-        ap_activity.process
+      DistributedMutex.synchronize("activity_pub_process_#{process_id}", validity: 2.minutes) do
+        ap_activity = DiscourseActivityPub::AP::Activity.factory(@args[:json])
+
+        if ap_activity && ap_activity.respond_to?(:process)
+          ap_activity.delivered_to << @args[:delivered_to] if @args[:delivered_to].present?
+          log_process_start
+          ap_activity.process
+        end
       end
     end
 
     protected
+
+    def process_id
+      @process_id ||=
+        begin
+          identifier =
+            DiscourseActivityPub::JsonLd.base_object_id(@args[:json]) ||
+              identifier = @args[:json].to_s if identifier.blank? || !identifier.is_a?(String)
+          Digest::MD5.hexdigest(identifier)
+        end
+    end
 
     def log_process_start
       DiscourseActivityPub::Logger.info(
