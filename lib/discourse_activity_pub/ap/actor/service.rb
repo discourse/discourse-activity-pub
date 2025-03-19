1662918014318b0a819c15db955304d9e4ef8115
Angus McLeod <angus@mcleod.org.au>
2025-03-19 14:15:02 +0100

    FEATURE: Add support for Service actors

--- lib/discourse_activity_pub/ap/actor/service.rb
+++ lib/discourse_activity_pub/ap/actor/service.rb
@@ -9,11 +9,20 @@ module DiscourseActivityPub
         end
 
         def can_belong_to
-          %i[]
+          %i[remote]
         end
 
         def can_perform_activity
-          {}
+          {
+            accept: [:follow],
+            reject: [:follow],
+            follow: [:group],
+            undo: %i[follow],
+            create: %i[note article],
+            update: %i[note article],
+            delete: %i[note article],
+            announce: %i[note article],
+          }
         end
       end
     end
