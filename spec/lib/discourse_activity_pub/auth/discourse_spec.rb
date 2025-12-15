dd177c1496fe00f17919d82e62522b8a2ae5950e
Penar Musaraj <pmusaraj@gmail.com>
2025-12-15 14:42:26 -0500

    DEV: Add user agent to auth requests as well (#269)
    Followup to 3bb5f08

--- spec/lib/discourse_activity_pub/auth/discourse_spec.rb
+++ spec/lib/discourse_activity_pub/auth/discourse_spec.rb
@@ -27,6 +27,11 @@ RSpec.describe DiscourseActivityPub::Auth::Discourse do
     opts[:body] = body.to_json if body
     opts[:headers] = {}
     opts[:headers]["Content-Type"] = "application/json" if body
+    if SiteSetting.activity_pub_send_user_agent
+      opts[:headers][
+        "User-Agent"
+      ] = "Discourse-ActivityPub/#{Discourse::VERSION::STRING} (+#{Discourse.base_url})"
+    end
     headers.each { |k, v| opts[:headers][k] = v } if headers
 
     Excon.expects(:send).with(verb, "https://#{domain}/#{path}", opts).returns(response)
