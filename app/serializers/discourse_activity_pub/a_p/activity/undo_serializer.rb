947a6ed16743b63552cca565fbfc2306757dc860
Penar Musaraj <pmusaraj@gmail.com>
2026-06-16 15:17:27 -0400

    SECURITY: Undo activity JSON exposes liked post content after ActivityPub publishing is disabled (#312)

--- app/serializers/discourse_activity_pub/a_p/activity/undo_serializer.rb
+++ app/serializers/discourse_activity_pub/a_p/activity/undo_serializer.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class DiscourseActivityPub::AP::Activity::UndoSerializer < DiscourseActivityPub::AP::ActivitySerializer
+  def _object
+    return super if DiscourseActivityPub.publishing_enabled
+
+    object.object&.id
+  end
 end
